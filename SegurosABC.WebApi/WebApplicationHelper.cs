ada37ce042b6f29bc93b5c1e85df08c9d82f9ac5
Jonny Rojas del Río <jonnyr@nserio.com>
2022-10-08 23:12:04 -0500

    project angular completed

--- SegurosABC.WebApi/WebApplicationHelper.cs
+++ SegurosABC.WebApi/WebApplicationHelper.cs
@@ -2,11 +2,15 @@
 {
 	public static class WebApplicationHelper
 	{
+		private const string _MY_ALLOW_SPECIFI_CORIGINS = "myAllowSpecificOrigins";
+
 		public static WebApplication CreateWebApplication(this WebApplicationBuilder builder)
 		{
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
 			builder.Services.AddSegurosABCDependencies(builder.Configuration, "SegurosABC");
+
+			builder.Services.AddCors();
 
 			return builder.Build();
 		}
@@ -14,6 +18,11 @@
 		public static WebApplication ConfigureWebApplication(this WebApplication app)
 		{
 			app.RunMigrations();
+			app.UseCors(x => x
+			   .AllowAnyMethod()
+			   .AllowAnyHeader()
+			   .SetIsOriginAllowed(origin => true) // allow any origin
+			   .AllowCredentials());
 
 			if (app.Environment.IsDevelopment())
 			{
